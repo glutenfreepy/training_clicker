3f91125532356b194d7020211ca4005337f65366
edg <glutenfreepy@gmail.com>
2021-05-11 15:08:06 -0700

    add user prompts, improve readme

--- boring_training_clicker.py
+++ boring_training_clicker.py
@@ -3,15 +3,23 @@ import pyautogui
 # get mouse coordinates
 print("First we need to know where the 'Next' button of your video is.")
 print("Please hold your mouse over the 'Next' button of the boring video.")
+input("Press Enter when ready")
+
+# do a countdown here
+# 3
+# 2
+# 1
 mouse_loc = pyautogui.position()
+clicks = 1000
+interval = 10
+button = 'left'
 
 # send mouse to coordinates
- pyautogui.moveTo(mouse_loc)
+pyautogui.moveTo(mouse_loc)
 
 # instead we can use click() to go to the coords before clicking
 # call click funtion to click next on the video and advance the lesson slide
-pyautogui.click(clicks=100, interval=10, button='left')
+pyautogui.click(clicks=clicks, interval=interval, button=button)
 
 print('pyautogui process is complete')
-
-
+#prompt to run again
